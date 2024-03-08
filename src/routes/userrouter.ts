bd71affd5d01eb44e3ba36eac4cb7ef93b46a15c
harshit <harshitshrivastava340@gmail.com>
2024-03-09 04:36:24 +0530

    validator

--- src/routes/userrouter.ts
+++ src/routes/userrouter.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { getAllUser ,signUpUser} from "../controller/user-routes.js";
+import {signupValidator, validate} from "../utils/validators.js"
+
 const userRouter = Router();
 
 userRouter.get("/",getAllUser);
-userRouter.post("/signup",signUpUser);
+userRouter.post("/signup",validate(signupValidator),signUpUser);
 
 export default userRouter;
